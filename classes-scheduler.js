373f0d2da727a4a38e2cb525186abe6fe645c596
AhmedBaba95 <ahmedbabamohamed95@gmail.com>
2023-10-26 16:31:04 +0000

    scheduleClass function

--- classes-scheduler.js
+++ classes-scheduler.js
@@ -1,29 +1,39 @@
-const { error } = require('console');
+// Import the required libraries
 const readline = require('readline');
+const moment = require('moment');
 
+// Create a readline interface for user input
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout,
 });
 
+// Define a function to get user input
 function getUserInput() {
+    // Use readline to ask the user for input
     rl.question('Enter the number of classes you want per week (2-4): ', (input) => {
-        // Use a try-catch block to check if the input is a number
         try {
+            // Attempt to parse the input as an integer
             const numberOfClasses = parseInt(input);
-  
+
+            // Check if the input is a single digit between 2 and 4
             if (input.length !== 1 || numberOfClasses < 2 || numberOfClasses > 4) {
                 console.log('Input is invalid. Please enter a single digit between 2 and 4.');
                 getUserInput(); // Ask for input again
                 return;
             }
-        
+
+            // Check if the input is not a number
             if (isNaN(numberOfClasses)) {
-                throw new Error('Input is not a number.');
+                throw new Error('Error: Input is not a number.');
             }
-  
-            // If the input is valid, log it and close the readline interface
+
+            // If the input is valid, log it
             console.log(`You entered ${numberOfClasses} classes per week.`);
+
+            // Calculate and schedule the class dates for the next four weeks
+            scheduleClasses(numberOfClasses);
+
             rl.close();
         } catch (error) {
             console.log(error.message);
@@ -31,6 +41,43 @@ function getUserInput() {
         }
     });
 }
-  
+
+let scheduledClasses = 0; // Initialize the class count
+
+function scheduleClasses(numberOfClasses) {
+    // Initialize the current date, starting from the next Monday
+    let currentDate = moment().startOf('week').add(7, 'days');
+
+    // Indicate that classes are being scheduled
+    console.log(`Class Schedule for the next four weeks:`);
+
+    for (let week = 1; week <= 4; week++) {
+        console.log(`Week ${week}:`);
+        
+        // Initialize a counter for classes scheduled in the current week
+        let classesScheduledThisWeek = 0;
+
+        // Enter a loop to schedule classes for the current week
+        while (classesScheduledThisWeek < numberOfClasses) {
+            // Check if the current day is a weekday (Monday to Friday)
+            if (currentDate.day() >= 1 && currentDate.day() <= 5) {
+                // Increment the scheduled class count for the current week
+                classesScheduledThisWeek++;
+
+                // Increment the total scheduled class count
+                scheduledClasses++;
+
+                // Print the class number, the formatted date, and the day of the week
+                console.log(`Class ${scheduledClasses}: ${currentDate.format('DD-MM-YYYY')}   (${currentDate.format('dddd')})`);
+            }
+
+            // Move the date to the next day for scheduling the next class
+            currentDate.add(1, 'day');
+        }
+
+        // Move to the next Monday for the next week
+        currentDate = currentDate.add(3, 'days');
+    }
+}
 
 getUserInput();
